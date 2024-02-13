8a900d8a7722164b091beaed46489a89c19701d8
Антон Валиев <tosha.valiev.91@mail.ru>
2024-02-13 23:01:05 +0500

    AddTestProject Добавлен проект с тестами и тест для примера

---
+++ Tests/SQRTTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQRT.Services;
+
+namespace Tests
+{
+    public class SQRTTests
+    {
+        /// <summary>
+        /// Пример теста
+        /// Создаем сервис
+        /// Assert - метод проверяющий условие
+        /// Почитать: На метаните есть статья посвященная xUnit
+        /// </summary>
+        [Fact]
+        public void ExampleTest()
+        {
+            var service = new SqrtWorker();
+
+            Assert.True(true);
+        }
+    }
+}
