cb6252c258d6e1338c42fabf5132bbcd14048a1e
Арсений Полынский <M0VeM0Ve@outlook.com>
2024-02-16 02:47:30 +0500

    Service fixed

--- SQRT/Services/SqrtWorker.cs
+++ SQRT/Services/SqrtWorker.cs
@@ -17,40 +17,41 @@ namespace SQRT.Services
             {
                 return "Введите число в корректном формате";
             }
-            else if (flag = Double.TryParse(input, out double result))
+            else
             {
                 input = input.Replace(" ", "");
                 input = input.Replace(".", ",");
-                Complex complexNumber = new Complex(result, 0);
-                Complex resultComplex = Complex.Sqrt(complexNumber);
-                string resultString = ConvertComplexToString(resultComplex, precision);
-                return resultString;
-            }
-            else if (IsComplexNumber(input))
-            {
-                input = input.Replace(" ", "");
-                input = input.Replace(".", ",");
-                input = input.Replace("i", "");
-                if (input.Contains("+"))
+                if (flag = Double.TryParse(input, out double result))
                 {
-                    string[] parts = input.Split('+');
-                    Complex complexNumber = new Complex(Double.Parse(parts[0]), Double.Parse(parts[1]));
+                    Complex complexNumber = new Complex(result, 0);
                     Complex resultComplex = Complex.Sqrt(complexNumber);
                     string resultString = ConvertComplexToString(resultComplex, precision);
                     return resultString;
+                }
+                else if (IsComplexNumber(input))
+                {
+                    input = input.Replace("i", "");
+                    if (input.Contains("+"))
+                    {
+                        string[] parts = input.Split('+');
+                        Complex complexNumber = new Complex(Double.Parse(parts[0]), Double.Parse(parts[1]));
+                        Complex resultComplex = Complex.Sqrt(complexNumber);
+                        string resultString = ConvertComplexToString(resultComplex, precision);
+                        return resultString;
+                    }
+                    else
+                    {
+                        string[] parts = input.Split('-');
+                        Complex complexNumber = new Complex(Double.Parse(parts[0]), Double.Parse(parts[1]) * -1);
+                        Complex resultComplex = Complex.Sqrt(complexNumber);
+                        string resultString = ConvertComplexToString(resultComplex, precision);
+                        return resultString;
+                    }
                 }
                 else
                 {
-                    string[] parts = input.Split('-');
-                    Complex complexNumber = new Complex(Double.Parse(parts[0]), Double.Parse(parts[1]) * -1);
-                    Complex resultComplex = Complex.Sqrt(complexNumber);
-                    string resultString = ConvertComplexToString(resultComplex, precision);
-                    return resultString;
+                    return "Введите число в корректном формате";
                 }
-            }
-            else
-            {
-                return "Введите число в корректном формате";
             }
         }
         public bool IsComplexNumber(string input)
